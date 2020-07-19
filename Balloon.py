b10e59d576e40e9cc71337bac038c5497d6ea7b9
Bejjo <Aldhiya.rozak@gmail.com>
2020-07-19 16:35:57 +0700

    second

--- Balloon.py
+++ Balloon.py
@@ -24,6 +24,8 @@ def printMap():
         for column in range(xmap):
             print(map[row][column],end="")
         print("")
+    infor()
+    print("\nResize Your Command Prompt")
 def moveHero(move,eat):
     global y,x
     if move == "a":
@@ -93,7 +95,7 @@ def play(name,best):
 
         cls()
 
-        print(f"Point : {point}\tRage : {rage}\tName : {name}\tBest Score : {best}")
+        print(f"Point($) : {point}\tRage : {rage}\tName : {name}\tBest Score : {best}")
         if rage != 0:
             rage-=1
 
@@ -116,20 +118,115 @@ def play(name,best):
 
         printMap()
     return point
+def infor():
+    print("A : Left\tS : Down\tD : Right\tW : Up")
+def findDollar():
+    for row in range(ymap):
+        for column in range(xmap):
+            if map[row][column]=="$":
+                return row,column
+mark2=False
+def checkBot(move):
+    global mark2
+    ybot,xbot = findDollar()
+    key=["da","sw"]
+    print(xbot,ybot)
+    if mark2:
+        if move=="a":
+            if xbot-x<0:
+                move = "a"
+            elif ybot-y>0:
+                move = "s"
+            elif ybot-y<0:
+                move = "w"
+        elif move=="s":
+            if xbot-x<0:
+                move = "a"
+            elif ybot-y>0:
+                move = "s"
+            elif xbot-x>0:
+                move = "d"
+        elif move=="d":
+            if ybot-y>0:
+                move = "s"
+            elif xbot-x>0:
+                move = "d"
+            elif ybot-y<0:
+                move = "w"
+        elif move=="w":
+            if xbot-x<0:
+                move = "a"
+            elif xbot-x>0:
+                move = "d"
+            elif ybot-y<0:
+                move = "w"
+    else:
+        if xbot-x<0:
+            move = "a"
+        elif ybot-y>0:
+            move = "s"
+        elif xbot-x>0:
+            move = "d"
+        elif ybot-y<0:
+            move = "w"
+    mark2=False
+    if mapCheck(move):
+        mark=0
+        if move=="d" or move=="a":
+            mark=1
+        for mv in key[mark]:
+            if mapCheck(mv)==False:
+                if mv=="a" and xbot-x<=0:
+                    move="a"
+                elif mv=="s" and ybot-y>=0:
+                    move="s"
+                elif mv=="d" and xbot-x>=0:
+                    move="d"
+                elif mv=="w" and ybot-y<=0:
+                    move="w"
+                mark2=True
+
+    return move
+def bot(name="Bot",best="9999"):
+    rage = 50
+    point = 0
+    map[2][10] = "$"
+    move = ""
+    borderMap()
+    while True:
+
+        cls()
+
+        print(f"Point($) : {point}\tRage : {rage}\tName : {name}\tBest Score : {best}")
+        if rage != 0:
+            rage-=1
+
+        move = checkBot(move)
+
+        check = mapCheck(move)
+        if check:
+            break
+
+        check2 = eatCheck(move)
+        moveHero(move,check2)
+        if check2:
+            point += 20
+            point += rage
+            rage = 50
+            dropDollar()
+
+        printMap()
+    return point
 def main():
-    name=""
-    best=0
+    global xmap,ymap,x,y,map
     scores=[]
-    lead={
-        "Pro":1000,
-        "Advance":600,
-        "Intermediate":300,
-        "Novice":100
-    }
+    name=""
+    allscores = [1000,600,300,100]
+    allnames = ["Pro(Computer)","Advance(Computer)","Intermediate(Computer)","Novice(Computer)"]
     while True:
         cls()
         print("Hello Ballons\n-------------")
-        print("1.Play\n2.Recent Score\n3.Leaderboard\n?.Details\n4.Exit")
+        print("1.Play\n2.Recent Score\n3.Leaderboard\n4.Details\n5.Bot\n6.Exit")
         print("Choice : ")
         asd = msvcrt.getch().decode("utf-8")
         if asd=="1":
@@ -138,8 +235,6 @@ def main():
             scores=[]
             name = input("Your Name : ")
             while True:
-                global xmap,ymap,x,y,map
-
                 xmap = 80
                 ymap = 20
                 x = 4
@@ -153,20 +248,47 @@ def main():
                 scores.append(score)
 
                 printMap()
-                print("Game Over")
+                print("\nGame Over")
                 print("Press [n] to back")
                 if msvcrt.getch().decode("utf-8").lower() == "n":
                     break
+            allscores.append(best)
+            allnames.append(name)
         elif asd=="2":
+            print("\nLast Played\n-----------")
             print(name,scores)
+            print("Press any key")
             msvcrt.getch()
         elif asd=="3":
-            for y,x in enumerate(sorted(lead, key=lead.get, reverse=True)):
-                print(y+1,x, lead[x])
+            print("\nLocal Rank\n----------")
+            for y,x in enumerate(sorted(allscores,reverse=True)):
+                p = allscores.index(x)
+                print(f"{y+1}. {allscores[p]} : {allnames[p]}")
+            print("Press any key")
             msvcrt.getch()
         elif asd=="4":
+            print("\nInformation\n-----------")
+            print("Dont hit the spike!!")
+            print("When the balloon hit the dollars, it will spawn a spike before\nExample : ")
+            print("\n* * *          * * *")
+            print("O $     ->     * O")
+            print("* * *          * * *\n")
+            print("\nMove Keys :")
+            infor()
+            print("\nPress any key")
+            msvcrt.getch()
+        elif asd=="5":
+            xmap = 80
+            ymap = 20
+            x = 4
+            y = 2
+            map = [[" " for x in range(xmap)] for x in range(ymap)]
+            map[y][x] = "O"
+            bot()
+            printMap()
+            msvcrt.getch()
+        elif asd=="6":
             break
-        lead[name] = best
 
 if __name__ == '__main__':
     main()
